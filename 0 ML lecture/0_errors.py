d8c887dbf4ab4e9b309dc2271993db9f160dd12a
Biswajeet Pradhan <biswajeetpradhan36933@gmail.com>
2026-01-12 22:40:54 +0000

    12-01-2026

--- 0 ML lecture/0_errors.py
+++ 0 ML lecture/0_errors.py
@@ -1,5 +1,5 @@
 import sys
-from dbhelper import DBhelper
+from _0_dbhelper import DBhelper
 
 class Facebook:
     def __init__(self):
@@ -8,11 +8,12 @@ class Facebook:
         self.menu()
 
     def menu(self):
-        user_input = input(
-            """1. Enter 1 to register
+        user_input = input("""
+            1. Enter 1 to register
             2. Enter 2 to login
-            3. Anything else to leave"""
-        )
+            3. Anything else to leave
+                           
+""")
 
         if user_input == "1":
             self.register()
@@ -22,6 +23,28 @@ class Facebook:
             sys.exit(1000)
 
 
-Facebook()
+    def register(self):
+        name = input('ENTER THE NAME : ')
+        email = input('ENTER THE E-MAIL : ')
+        password = input('ENTER THE PASSWORD : ')
 
+        response = self.db.register(name, email, password)
+
+        if response:
+            print("REGISTRATION SUCCESSFUL")
+        else:
+            print("REGISTRATION FAILED")
+
+        self.menu()
+
+    def login(self):
+        # Placeholder for login logic
+        print("LOGIN FUNCTIONALITY NOT IMPLEMENTED YET")
+        self.menu()
+
+# **********************************************************************************************************************************************
+
+obj = Facebook()
+
+# AFTER SUCCESSFULLY ADDING A NEW USER, IT WILL ADD THAT USER'S DETAILS INTO THE DATABASE AT 'phpMyAdmin'
 
