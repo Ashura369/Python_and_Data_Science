22787f7e7d0f7fac500a06f27e93c4cdbba0588e
ashura369 <biswajeetpradhan36933@gmail.com>
2026-07-25 09:57:35 +0530

    25-07-2026

--- 0 ML lecture/flask/project 3/app.py
+++ 0 ML lecture/flask/project 3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.secret_key = "pradhans284"              # setting up secret key
@@ -16,10 +16,26 @@ def login():
     return render_template('2_login_page.html')
 
 # -------------------------------------------------------------------------------------------
+# getting the login information
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_name = request.form.get('username')        # flask receives the 'username' bcoz the name is set 'username' for the name input field
+    user_email = request.form.get('email')
 
+    # putting admin previlage
+    admins={
+        'name' : 'admin',
+        'email' : 'admin@123'
+    }
 
+    admin_html = f"<h2>Hello {user_name}</h2>"
 
-
+    if user_name in admins['name'] and user_email in admins['email']:
+        return render_template('3_page.html', name=user_name, email=user_email)
+    elif user_name and user_email:
+        return render_template('3_page.html', name=user_name, email=user_email)
+    else:
+        return render_template('2_login_page.html')
 
 
 # -------------------------------------------------------------------------------------------
